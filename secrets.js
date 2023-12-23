d6aeb1acd5ddc4e9bcdd018098cbc77054363f74
j-maestre <xemaiestacio@gmail.com>
2023-12-23 21:02:34 +0100

    add google maps coordinates

--- secrets.js
+++ secrets.js
@@ -6,6 +6,16 @@ const clavePadel ="Pepe botella"
 const claveLaberinto = 19
 const claveSpoty = "Mondongo"
 
+const map1_coord = "38.7997903,-0.6120728";
+const map2_coord = "38.8206086,-0.597979";
+const map3_coord = "41.6527779,-0.8801563";
+
+
+const maps_1_link = "https://www.google.es/maps/@38.7997903,-0.6120728,3a,75y,157.62h,72t/data=!3m8!1e1!3m6!1sAF1QipPqYS6fkU8QhQrFo10LOeaE4MS5fqcn3yYbPJG4!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipPqYS6fkU8QhQrFo10LOeaE4MS5fqcn3yYbPJG4%3Dw203-h100-k-no-pi-0-ya182.22455-ro0-fo100!7i6000!8i3000?entry=ttu";
+const maps_2_link = "https://www.google.es/maps/@38.8206086,-0.597979,3a,75y,307.63h,90.86t/data=!3m7!1e1!3m5!1sl1dXQeCmTyJACwyAg_hiDg!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3Dl1dXQeCmTyJACwyAg_hiDg%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D97.51202%26pitch%3D0%26thumbfov%3D100!7i13312!8i6656?entry=ttu";
+const maps_3_link = "https://www.google.com/maps/@41.6527779,-0.8801563,3a,75y,318.45h,101.26t/data=!3m6!1e1!3m4!1s3XT7n51Gfj6LDIDg2upeaw!2e0!7i13312!8i6656?entry=ttu";
+
+
 const clave1 = "https://"
 const clave2 = ""
 const clave3 = ""
